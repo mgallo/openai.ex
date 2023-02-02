97003c2fa282f4ea4b0e6b5b3046b224b95d9b55
mgallo <mgallo@linux.com>
2023-02-02 23:19:06 +0100

    chore: bump beta version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Openai.MixProject do
   def project do
     [
       app: :openai,
-      version: "0.2.3",
+      version: "0.3.0-beta",
       elixir: "~> 1.11",
       description: description(),
       start_permanent: Mix.env() == :prod,
