47800ca58d71b3cf06054c72926811e8164abd52
mgallo <mgallo@linux.com>
2023-02-04 18:05:59 +0100

    feat: add support to missing finetunes API

--- lib/openai/finetunes.ex
+++ lib/openai/finetunes.ex
@@ -6,6 +6,8 @@ defmodule OpenAI.Finetunes do
 
   def url(), do: @finetuning_base_url
   def url(finetune_id), do: "#{@finetuning_base_url}/#{finetune_id}"
+  def cancel_url(finetune_id), do: "#{@finetuning_base_url}/#{finetune_id}/cancel"
+  def events_url(finetune_id), do: "#{@finetuning_base_url}/#{finetune_id}/events"
 
   def fetch(finetune_id) do
     url(finetune_id)
@@ -14,6 +16,21 @@ defmodule OpenAI.Finetunes do
 
   def fetch() do
     url()
+    |> Client.api_get()
+  end
+
+  def create(params) do
+    url()
+    |> Client.api_post(params)
+  end
+
+  def cancel(finetune_id) do
+    cancel_url(finetune_id)
+    |> Client.api_post()
+  end
+
+  def list_events(finetune_id) do
+    events_url(finetune_id)
     |> Client.api_get()
   end
 end
