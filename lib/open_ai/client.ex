a0dc6c9839b70165a37f627243ca6b91f224b92d
TheMarinac <nikola@regex-sh.com>
2023-03-19 18:11:24 +0100

    feat(client): Add get post and delete fns
    wip: Mutlipart POST
    Signed-off-by: TheMarinac <nikola@regex-sh.com>

---
+++ lib/open_ai/client.ex
@@ -0,0 +1,37 @@
+defmodule OpenAi.Client do
+  @moduledoc """
+  Client module for OpenAI API
+  """
+
+  use Tesla
+  alias Tesla.Multipart
+
+  plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.BaseUrl, "https://api.openai.com"
+  plug Tesla.Middleware.BearerAuth, token: Application.get_env(:openai, :api_key)
+  plug Tesla.Middleware.Timeout, timeout: Application.get_env(:openai, :http_timeout)
+
+  plug Tesla.Middleware.Headers, [
+    {"OpenAI-Organization", Application.get_env(:openai, :organization_key)}
+  ]
+
+  def api_get(url, request_options \\ []) do
+    get(url, request_options)
+  end
+
+  def api_post(url, params \\ [], request_options \\ []) do
+    post(url, params, request_options)
+  end
+
+  def api_delete(url, request_options \\ []) do
+    delete(url, request_options)
+  end
+
+  def multipart_api_post(url, file_path, file_param, params, request_options \\ []) do
+    multipart =
+      Multipart.new()
+      |> Multipart.add_file(file_param, file_path)
+
+    post(url, multipart, request_options)
+  end
+end
