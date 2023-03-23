8a70ace6bca87a03a327a1ea196df455ed409b8f
Ryan Daigle <ryan.daigle@gmail.com>
2023-03-23 13:41:22 -0400

    Expose OpenAI URL as a config option
    Resolves: https://github.com/mgallo/openai.ex/issues/20

--- test/config_test.exs
+++ test/config_test.exs
@@ -1,6 +1,7 @@
 defmodule OpenAI.ConfigTest do
   use ExUnit.Case
   alias OpenAI.Config
+  alias OpenAI.Client
 
   @application :openai
 
@@ -14,6 +15,14 @@ defmodule OpenAI.ConfigTest do
 
     Application.put_env(@application, :http_options, recv_timeout: 30_000)
     assert Config.http_options() == [recv_timeout: 30_000]
+  end
+
+  test "api_url option is recognized" do
+    assert Config.api_url == "https://api.openai.com"
+
+    Application.put_env(@application, :api_url, "http://localhost:8080")
+    assert Config.api_url == "http://localhost:8080"
+		assert Client.process_request_url("/api") == "http://localhost:8080/api"
   end
 
   defp reset_env() do
