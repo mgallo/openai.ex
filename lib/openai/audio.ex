388baf1d755aa84d080c4d4ab4366f330ab43715
mgallo <mgallo@linux.com>
2023-04-22 02:22:23 +0200

    feat: add default to audio methods

--- lib/openai/audio.ex
+++ lib/openai/audio.ex
@@ -1,17 +1,18 @@
 defmodule OpenAI.Audio do
   @moduledoc false
   alias OpenAI.Client
+  alias OpenAI.Config
 
   @base_url "/v1/audio"
   def transcriptions_url(), do: "#{@base_url}/transcriptions"
   def translations_url(), do: "#{@base_url}/translations"
 
-  def transcription(file_path, params, config) do
+  def transcription(file_path, params, config \\ %Config{}) do
     transcriptions_url()
     |> Client.multipart_api_post(file_path, "file", params, config)
   end
 
-  def translation(file_path, params, config) do
+  def translation(file_path, params, config \\ %Config{}) do
     translations_url()
     |> Client.multipart_api_post(file_path, "file", params, config)
   end
