fb69a7ae6d79d19a4b6618f05578336aa6832704
mgallo <mgallo@linux.com>
2023-05-02 19:03:28 +0200

    chore: bump version v0.5.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Openai.MixProject do
   def project do
     [
       app: :openai,
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.11",
       description: description(),
       start_permanent: Mix.env() == :prod,
