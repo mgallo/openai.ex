92d7ee1be290e5310b09b098494fe7180520ef5a
Brad Folkens <2154+bfolkens@users.noreply.github.com>
2023-09-10 07:44:24 -0500

    fix: handle HTTPoison error case during Stream.new/1 - Fixes #51 (#52)

--- lib/openai/stream.ex
+++ lib/openai/stream.ex
@@ -4,53 +4,68 @@ defmodule OpenAI.Stream do
   def new(start_fun) do
     Stream.resource(
       start_fun,
-      fn res ->
-        {res, id} = case res do
-          {:ok, res = %HTTPoison.AsyncResponse{id: id}} -> {res, id}
-          res = %HTTPoison.AsyncResponse{id: id} -> {res, id}
-        end
+      fn
+        {:error, %HTTPoison.Error{} = error} ->
+          {
+            [
+              %{
+                "status" => :error,
+                "reason" => error.reason,
+              }
+            ],
+            error
+          }
 
-        receive do
-          %HTTPoison.AsyncStatus{id: ^id, code: code} ->
-            HTTPoison.stream_next(res)
-            case code do
-              200 ->
-                {[], res}
-              _ ->
-                {
-                  [
-                    %{
-                      "status" => :error,
-                      "code" => code,
-                      "choices" => []
-                    }
-                  ],
-                  res
-                }
-            end
-          %HTTPoison.AsyncHeaders{id: ^id, headers: _headers}->
-            HTTPoison.stream_next(res)
-            {[], res}
-          %HTTPoison.AsyncChunk{chunk: chunk}->
-            data =
-              chunk
-              |> String.split("\n")
-              |> Enum.filter(fn line ->
-                String.starts_with?(line, "data: {")
-              end)
-              |> Enum.map(fn line ->
-                line
-                |> String.replace_prefix("data: ", "")
-                |> Jason.decode!()
-              end)
+        %HTTPoison.Error{} = error ->
+          {:halt, error}
 
-            HTTPoison.stream_next(res)
-            {data, res}
-          %HTTPoison.AsyncEnd{}->
-            {:halt, res}
-        end
+        res ->
+          {res, id} = case res do
+            {:ok, res = %HTTPoison.AsyncResponse{id: id}} -> {res, id}
+            res = %HTTPoison.AsyncResponse{id: id} -> {res, id}
+          end
+
+          receive do
+            %HTTPoison.AsyncStatus{id: ^id, code: code} ->
+              HTTPoison.stream_next(res)
+              case code do
+                200 ->
+                  {[], res}
+                _ ->
+                  {
+                    [
+                      %{
+                        "status" => :error,
+                        "code" => code,
+                        "choices" => []
+                      }
+                    ],
+                    res
+                  }
+              end
+            %HTTPoison.AsyncHeaders{id: ^id, headers: _headers}->
+              HTTPoison.stream_next(res)
+              {[], res}
+            %HTTPoison.AsyncChunk{chunk: chunk}->
+              data =
+                chunk
+                |> String.split("\n")
+                |> Enum.filter(fn line ->
+                  String.starts_with?(line, "data: {")
+                end)
+                |> Enum.map(fn line ->
+                  line
+                  |> String.replace_prefix("data: ", "")
+                  |> Jason.decode!()
+                end)
+
+              HTTPoison.stream_next(res)
+              {data, res}
+            %HTTPoison.AsyncEnd{}->
+              {:halt, res}
+          end
       end,
-      fn %HTTPoison.AsyncResponse{id: id} ->
+      fn %{id: id} ->
         id |> :hackney.stop_async()
       end
     )
