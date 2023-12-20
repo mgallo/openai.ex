d54ab22d45848b9eeeade0297240c9ab5fd513c9
David Snoble <49111966+DavidSnoble@users.noreply.github.com>
2023-12-20 08:47:07 -0700

    download file content

--- lib/openai/files.ex
+++ lib/openai/files.ex
@@ -7,6 +7,7 @@ defmodule OpenAI.Files do
 
   def url(), do: @files_base_url
   def url(file_id), do: "#{@files_base_url}/#{file_id}"
+  def content_url(file_id), do: "#{@files_base_url}/#{file_id}/content"
 
   def fetch(config \\ %Config{}) do
     url()
@@ -26,5 +27,10 @@ defmodule OpenAI.Files do
   def upload(file_path, params, config \\ %Config{}) do
     url()
     |> Client.multipart_api_post(file_path, "file", params, config)
+  end
+
+  def download(file_id, config \\ %Config{}) do
+    content_url(file_id) 
+    |> Client.api_get(config)
   end
 end
