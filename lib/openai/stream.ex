dc1942e8f33970cf1ec5cf1f2bd05879f636f75b
Bradford Folkens <i.am@bradfolkens.com>
2024-03-26 20:51:40 -0500

    Changed stream resource to handle accumulating chunks to avoid orphaned SSE events in chunks - Fixes #60

--- lib/openai/stream.ex
+++ lib/openai/stream.ex
@@ -2,6 +2,13 @@ defmodule OpenAI.Stream do
   @moduledoc false
 
   def new(start_fun) do
+    start_fun
+    |> build_stream()
+    |> parse_events()
+    |> parse_data()
+  end
+
+  defp build_stream(start_fun) do
     Stream.resource(
       start_fun,
       fn
@@ -10,7 +17,7 @@ defmodule OpenAI.Stream do
             [
               %{
                 "status" => :error,
-                "reason" => error.reason,
+                "reason" => error.reason
               }
             ],
             error
@@ -20,17 +27,20 @@ defmodule OpenAI.Stream do
           {:halt, error}
 
         res ->
-          {res, id} = case res do
-            {:ok, res = %HTTPoison.AsyncResponse{id: id}} -> {res, id}
-            res = %HTTPoison.AsyncResponse{id: id} -> {res, id}
-          end
+          {res, id} =
+            case res do
+              {:ok, res = %HTTPoison.AsyncResponse{id: id}} -> {res, id}
+              res = %HTTPoison.AsyncResponse{id: id} -> {res, id}
+            end
 
           receive do
             %HTTPoison.AsyncStatus{id: ^id, code: code} ->
               HTTPoison.stream_next(res)
+
               case code do
                 200 ->
                   {[], res}
+
                 _ ->
                   {
                     [
@@ -43,31 +53,56 @@ defmodule OpenAI.Stream do
                     res
                   }
               end
-            %HTTPoison.AsyncHeaders{id: ^id, headers: _headers}->
+
+            %HTTPoison.AsyncHeaders{id: ^id, headers: _headers} ->
               HTTPoison.stream_next(res)
               {[], res}
-            %HTTPoison.AsyncChunk{chunk: chunk}->
-              data =
-                chunk
-                |> String.split("\n")
-                |> Enum.filter(fn line ->
-                  String.starts_with?(line, "data: {")
-                end)
-                |> Enum.map(fn line ->
-                  line
-                  |> String.replace_prefix("data: ", "")
-                  |> Jason.decode!()
-                end)
 
+            %HTTPoison.AsyncChunk{chunk: chunk} ->
               HTTPoison.stream_next(res)
-              {data, res}
-            %HTTPoison.AsyncEnd{}->
+              {[chunk], res}
+
+            %HTTPoison.AsyncEnd{} ->
               {:halt, res}
           end
       end,
       fn %{id: id} ->
-        id |> :hackney.stop_async()
+        :hackney.stop_async(id)
       end
     )
+  end
+
+  defp parse_events(stream) do
+    Stream.transform(stream, "", fn
+      chunk, acc when is_binary(chunk) ->
+        case String.split(acc <> chunk, "\n\n") do
+          [] ->
+            {[], ""}
+
+          [tail] ->
+            {[], tail}
+
+          [_ | _] = events ->
+            {events, [tail]} = Enum.split(events, -1)
+            {events, tail}
+        end
+
+      data, _acc ->
+        {[data], ""}
+    end)
+  end
+
+  defp parse_data(stream) do
+    Stream.transform(stream, nil, fn
+      "data: [DONE]", acc ->
+        {:halt, acc}
+
+      "data: " <> data, acc ->
+        data = Jason.decode!(data)
+        {[data], acc}
+
+      data, acc ->
+        {[data], acc}
+    end)
   end
 end
