5831571a5b41868c4f05ea60e15b89ec32a40d3b
Brad Folkens <2154+bfolkens@users.noreply.github.com>
2024-07-18 03:39:47 -0500

    fix: intermittent Jason.DecodeError in stream (#61)
    * Handle HTTPoison error case during Stream.new/1 - Fixes #51
    
    * Changed stream resource to handle accumulating chunks to avoid orphaned SSE events in chunks - Fixes #60
    
    * Fix hackney close interrupted by :halt further downstream
    
    * Ensure dangling accumulator case is handled
    
    * Handle OpenAI error case
    
    * Fixed issues with event: field not being recognized
    
    * Fixed test failure - bad return

--- lib/openai/stream.ex
+++ lib/openai/stream.ex
@@ -1,73 +1,84 @@
 defmodule OpenAI.Stream do
   @moduledoc false
 
+  alias OpenAI.SSEStreamParser
+
   def new(start_fun) do
+    start_fun
+    |> build_stream()
+    |> SSEStreamParser.parse()
+    |> decode_data()
+  end
+
+  defp build_stream(start_fun) do
     Stream.resource(
       start_fun,
       fn
+        {:ok, res = %HTTPoison.AsyncResponse{}} ->
+          {[], {nil, res}}
+
         {:error, %HTTPoison.Error{} = error} ->
-          {
-            [
-              %{
-                "status" => :error,
-                "reason" => error.reason,
-              }
-            ],
-            error
-          }
+          {[{:error, error}], :error}
 
         %HTTPoison.Error{} = error ->
-          {:halt, error}
+          {[{:error, error}], :error}
 
-        res ->
-          {res, id} = case res do
-            {:ok, res = %HTTPoison.AsyncResponse{id: id}} -> {res, id}
-            res = %HTTPoison.AsyncResponse{id: id} -> {res, id}
-          end
-
+        {code, res = %HTTPoison.AsyncResponse{id: id}} ->
           receive do
             %HTTPoison.AsyncStatus{id: ^id, code: code} ->
               HTTPoison.stream_next(res)
-              case code do
-                200 ->
-                  {[], res}
-                _ ->
-                  {
-                    [
-                      %{
-                        "status" => :error,
-                        "code" => code,
-                        "choices" => []
-                      }
-                    ],
-                    res
-                  }
-              end
-            %HTTPoison.AsyncHeaders{id: ^id, headers: _headers}->
-              HTTPoison.stream_next(res)
-              {[], res}
-            %HTTPoison.AsyncChunk{chunk: chunk}->
-              data =
-                chunk
-                |> String.split("\n")
-                |> Enum.filter(fn line ->
-                  String.starts_with?(line, "data: {")
-                end)
-                |> Enum.map(fn line ->
-                  line
-                  |> String.replace_prefix("data: ", "")
-                  |> Jason.decode!()
-                end)
+              {[], {code, res}}
 
+            # We should be able to tell the difference between an error and the
+            # event stream with content-type (text/event-stream), but
+            # unfortunately OpenAI doesn't obey the spec.
+            %HTTPoison.AsyncHeaders{id: ^id, headers: _headers} ->
               HTTPoison.stream_next(res)
-              {data, res}
-            %HTTPoison.AsyncEnd{}->
-              {:halt, res}
+              {[], {code, res}}
+
+            %HTTPoison.AsyncChunk{chunk: chunk} ->
+              HTTPoison.stream_next(res)
+              {[{code, chunk}], {code, res}}
+
+            %HTTPoison.AsyncEnd{id: ^id} ->
+              {:halt, {code, res}}
           end
+
+        :error ->
+          {:halt, :error}
       end,
-      fn %{id: id} ->
-        id |> :hackney.stop_async()
+      fn
+        {_code, %{id: id}} ->
+          :hackney.stop_async(id)
+
+        :error ->
+          :ok
       end
     )
+  end
+
+  defp decode_data(stream) do
+    stream
+    |> Stream.reject(fn
+      "[DONE]" -> true
+      "" -> true
+      _ -> false
+    end)
+    |> Stream.map(fn
+      data when is_binary(data) ->
+        case Jason.decode(data) do
+          {:ok, struct} ->
+            struct
+
+          {:error, _reason} ->
+            data
+        end
+
+      %HTTPoison.Error{} = error ->
+        %{"reason" => error.reason, "status" => :error}
+
+      data ->
+        data
+    end)
   end
 end
--- test/openai/stream_test.exs
+++ test/openai/stream_test.exs
@@ -1,8 +1,13 @@
 defmodule OpenAI.StreamTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  setup do
+    bypass = Bypass.open()
+    %{bypass: bypass, url: "http://localhost:#{bypass.port}"}
+  end
 
   describe "new/1" do
-    test "handles error" do
+    test "handles connection error" do
       results =
         OpenAI.Stream.new(fn ->
           HTTPoison.get("http://nxdomain-error/", [], stream_to: self(), async: :once)
@@ -10,6 +15,184 @@ defmodule OpenAI.StreamTest do
         |> Enum.to_list()
 
       assert [%{"reason" => :nxdomain, "status" => :error}] == results
+    end
+
+    test "handles OpenAI malformed error", %{bypass: bypass, url: url} do
+      Bypass.expect(bypass, "GET", "/", fn conn ->
+        conn =
+          conn
+          |> Plug.Conn.put_resp_header("content-type", "text/event-stream")
+          |> Plug.Conn.send_chunked(400)
+
+        Plug.Conn.chunk(conn, "{\n")
+        Plug.Conn.chunk(conn, ~s|"error": {\n|)
+
+        Plug.Conn.chunk(
+          conn,
+          ~s|"message": "The model `gpt-3.5` does not exist or you do not have access to it.",\n|
+        )
+
+        Plug.Conn.chunk(conn, ~s|"type": "invalid_request_error",\n|)
+        Plug.Conn.chunk(conn, ~s|"param": null,\n|)
+        Plug.Conn.chunk(conn, ~s|"code": "model_not_found"\n|)
+        Plug.Conn.chunk(conn, "}\n")
+        Plug.Conn.chunk(conn, "}\n")
+
+        conn
+      end)
+
+      results =
+        OpenAI.Stream.new(fn ->
+          HTTPoison.get(url, [], stream_to: self(), async: :once)
+        end)
+        |> Enum.to_list()
+
+      assert [
+               %{
+                 "error" => %{
+                   "code" => "model_not_found",
+                   "message" =>
+                     "The model `gpt-3.5` does not exist or you do not have access to it.",
+                   "param" => nil,
+                   "type" => "invalid_request_error"
+                 }
+               }
+             ] == results
+    end
+
+    test "handles stream with event name", %{bypass: bypass, url: url} do
+      Bypass.expect(bypass, "GET", "/", fn conn ->
+        conn =
+          conn
+          |> Plug.Conn.put_resp_header("content-type", "text/event-stream")
+          |> Plug.Conn.send_chunked(200)
+
+        Plug.Conn.chunk(
+          conn,
+          ~s|event: thread.run.created\ndata: {"id":"run_01","object":"thread.run","created_at":1721218990,"assistant_id":"asst_01","thread_id":"thread_01","status":"queued","started_at":null,"expires_at":1721219590,"cancelled_at":null,"failed_at":null,"completed_at":null,"required_action":null,"last_error":null,"model":"gpt-4o","instructions":"content","tools":[],"tool_resources":{"code_interpreter":{"file_ids":[]}},"metadata":{},"temperature":0.01,"top_p":1.0,"max_completion_tokens":null,"max_prompt_tokens":null,"truncation_strategy":{"type":"auto","last_messages":null},"incomplete_details":null,"usage":null,"response_format":"auto","tool_choice":"auto","parallel_tool_calls":true}\n\n|
+        )
+
+        conn
+      end)
+
+      results =
+        OpenAI.Stream.new(fn ->
+          HTTPoison.get(url, [], stream_to: self(), async: :once)
+        end)
+        |> Enum.to_list()
+
+      assert [
+               %{
+                 "assistant_id" => "asst_01",
+                 "cancelled_at" => nil,
+                 "completed_at" => nil,
+                 "created_at" => 1_721_218_990,
+                 "expires_at" => 1_721_219_590,
+                 "failed_at" => nil,
+                 "id" => "run_01",
+                 "incomplete_details" => nil,
+                 "instructions" => "content",
+                 "last_error" => nil,
+                 "max_completion_tokens" => nil,
+                 "max_prompt_tokens" => nil,
+                 "metadata" => %{},
+                 "model" => "gpt-4o",
+                 "object" => "thread.run",
+                 "parallel_tool_calls" => true,
+                 "required_action" => nil,
+                 "response_format" => "auto",
+                 "started_at" => nil,
+                 "status" => "queued",
+                 "temperature" => 0.01,
+                 "thread_id" => "thread_01",
+                 "tool_choice" => "auto",
+                 "tool_resources" => %{"code_interpreter" => %{"file_ids" => []}},
+                 "tools" => [],
+                 "top_p" => 1.0,
+                 "truncation_strategy" => %{"last_messages" => nil, "type" => "auto"},
+                 "usage" => nil
+               }
+             ] == results
+    end
+
+    test "handles chunked event stream", %{bypass: bypass, url: url} do
+      Bypass.expect(bypass, "GET", "/", fn conn ->
+        conn =
+          conn
+          |> Plug.Conn.put_resp_header("content-type", "text/event-stream")
+          |> Plug.Conn.send_chunked(200)
+
+        Plug.Conn.chunk(
+          conn,
+          ~s|data: {"id":"chatcmpl-01","object":"chat.completion.chunk","created":0,"model":"gpt-3.5-turbo-0125","system_fingerprint":"fp_deadbeef","choices":[{"index":0,"delta":{"content":" The"},"logprobs":null,"finish_reason":null}]}\n\n|
+        )
+
+        Plug.Conn.chunk(
+          conn,
+          ~s|data: {"id":"chatcmpl-02","object":"chat.completion.chunk","created":0,"model":"gpt-3.5-turbo-0125","system_fingerprint":"fp_deadbeef","choices":[{"index":0,"delta":{"content":" Test"},"logprobs":null,"finish_reason":null}]}\n\ndata: {"id":"chatcm|
+        )
+
+        Plug.Conn.chunk(
+          conn,
+          ~s|pl-03","object":"chat.completion.chunk","created":0,"model":"gpt-3.5-turbo-0125","system_fingerprint":"fp_deadbeef","choices":[{"index":0,"delta":{},"logprobs":null,"finish_reason":"stop"}]}\n\ndata: [DONE]|
+        )
+
+        conn
+      end)
+
+      results =
+        OpenAI.Stream.new(fn ->
+          HTTPoison.get(url, [], stream_to: self(), async: :once)
+        end)
+        |> Enum.to_list()
+
+      assert [
+               %{
+                 "id" => "chatcmpl-01",
+                 "object" => "chat.completion.chunk",
+                 "created" => 0,
+                 "model" => "gpt-3.5-turbo-0125",
+                 "system_fingerprint" => "fp_deadbeef",
+                 "choices" => [
+                   %{
+                     "index" => 0,
+                     "delta" => %{"content" => " The"},
+                     "logprobs" => nil,
+                     "finish_reason" => nil
+                   }
+                 ]
+               },
+               %{
+                 "id" => "chatcmpl-02",
+                 "object" => "chat.completion.chunk",
+                 "created" => 0,
+                 "model" => "gpt-3.5-turbo-0125",
+                 "system_fingerprint" => "fp_deadbeef",
+                 "choices" => [
+                   %{
+                     "index" => 0,
+                     "delta" => %{"content" => " Test"},
+                     "logprobs" => nil,
+                     "finish_reason" => nil
+                   }
+                 ]
+               },
+               %{
+                 "id" => "chatcmpl-03",
+                 "object" => "chat.completion.chunk",
+                 "created" => 0,
+                 "model" => "gpt-3.5-turbo-0125",
+                 "system_fingerprint" => "fp_deadbeef",
+                 "choices" => [
+                   %{
+                     "index" => 0,
+                     "delta" => %{},
+                     "logprobs" => nil,
+                     "finish_reason" => "stop"
+                   }
+                 ]
+               }
+             ] == results
     end
   end
 end
